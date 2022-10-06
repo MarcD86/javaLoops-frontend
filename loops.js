6d0d85034ffbbacec5cb0a30de4c6e0d8fa08eb1
MarcD86 <marc_duim@live.nl>
2022-10-06 20:32:50 +0200

    update1

--- loops.js
+++ loops.js
@@ -6,7 +6,9 @@
 // Hoera!
 // ==========================================
 
-
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera!")
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -17,7 +19,14 @@
 // loop...
 // klaar!
  // ==========================================
-
+for (let i = 0; i < 5; i++) {
+    if(i < 4) {
+        console.log("loop...")
+    }
+    else {
+        console.log("klaar!")
+    }
+}
 
 
 // ==========================================
@@ -32,7 +41,14 @@
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
-
+for (let i = 0; i <= 15; i++) {
+    if(i % 2 === 0 || i === 0) {
+        console.log(i + " is even")
+    }
+    else {
+        console.log(i + " is oneven")
+    }
+}
 
 
 // ==========================================
@@ -50,4 +66,14 @@
 // >> 8
 // >> 9
 // ==========================================
-
+for (let i = 0; i <= 9; i++) {
+    if(i < 3) {
+        console.log(i)
+    }
+    else if(i > 2 && i < 6) {
+        console.log("> " + i)
+    }
+    else {
+        console.log(">> " + i)
+    }
+}
